de5ad60895b33519bdd710ffc9cb065c9bd6ea61
Matisse TILL <matisse.till@student.junia.com>
2023-01-19 11:27:34 +0100

    jeudi matin

--- DjangoApp/Main/views.py
+++ DjangoApp/Main/views.py
@@ -1,11 +1,17 @@
+from email.errors import NoBoundaryInMultipartDefect
+from sre_constants import REPEAT_ONE
+from tkinter.messagebox import QUESTION
+from xml.dom import NoModificationAllowedErr
 from django.shortcuts import render
+from django.shortcuts import redirect
 import json
 import openai
 from .forms import QuestionForm
 from .models import Question
 import os
 
-openai.api_key = "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"
+
+openai.api_key = "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"
 
 def CallChatGpt(question,reponse):
     questionAposer = "Partons du principe que tu es un professeur ayant realise un examen. a la question comportant la consigne : {}. ton eleve a repondu : {}. est-ce que cette réponse est correcte ou incorrecte ? Réponds uniquement en un seul mot sans utilisation de points à la fin de ta phrase".format(question,reponse)
@@ -68,9 +74,8 @@ def Configuration(request):
 def Questionnaire(request):
     with open("media/question/data.json") as json_file:
         jsonfile = json.load(json_file)
-
     nombre_question=len(jsonfile["QCM"])
-
+    print(nombre_question)
     question=[]
     point=[]
     types=[]
@@ -78,7 +83,6 @@ def Questionnaire(request):
     reponse=[]
     explication=[]
     index=[]
-
     for o in range (nombre_question):
         question.append(jsonfile["QCM"]["Question {}".format(o+1)]["Question"])
         point.append(jsonfile["QCM"]["Question {}".format(o+1)]["Points"])
@@ -90,28 +94,50 @@ def Questionnaire(request):
         print("hello")
         ElementsQuestion=zip(question,point,types,choix,reponse,explication,index)
         context = {'ElementsQuestion':ElementsQuestion}
-
-    
-
-    if request.method == "POST":
-        Correction=[]
-        reponses = request.POST.dict()
-        for i in range (nombre_question):
-            print(reponses["Question{}".format(i+1)])
-            Correction.append(CallChatGpt(question[i],reponses["Question{}".format(i+1)]))
-        
-        #Correction = ["oui","oui"]
-        #print(Correction)
-        ElementsQuestion=zip(question,point,types,choix,reponse,explication,index,Correction)
-        context = {'ElementsQuestion':ElementsQuestion}
-    
-    
-
-
-
     return render(request, 'main/questionnaire.html',context)
 
 
+def Note(liste_reponse):
+    '''prend en argument une liste de [".Correcte",".Incorrecte"] de la longueure du nombre de question
+    retourne un liste avec [nombre de point, total points du questionnaire, note sur 20]'''
+
+    with open("media/question/data.json") as json_file:
+        data = json.load(json_file)
+
+    # access the QCM section
+    qcm_data = data['QCM']
+    total_points = []
+    bareme = 0
+    reponse = 0
+    for question in qcm_data.values() :
+        bareme += int(question['Points'])
+        if (liste_reponse[reponse]=='.\n\nCorrecte'):
+            total_points.append(int(question['Points']))
+        reponse += 1
+
+    return [sum(total_points),bareme,sum(total_points)*20/bareme]
+
+def Correction(request):
+    with open("media/question/data.json") as json_file:
+        jsonfile = json.load(json_file)
+    nombre_question=len(jsonfile["QCM"])
+    question=[]
+    reponse_list=[]
+    for o in range (nombre_question):
+        question.append(jsonfile["QCM"]["Question {}".format(o+1)]["Question"])
+    if request.method == "POST": 
+        Correction=[]
+        reponsePost = request.POST.dict()
+        for i in range (nombre_question):
+            Correction.append(CallChatGpt(question[i],reponsePost["Question{}".format(i+1)]))
+        for i in range (nombre_question):
+            reponse_list.append(reponsePost["Question{}".format(i+1)])
+        note=Note(Correction)[2]
+        ElementsQuestion=zip(question,Correction,reponse_list)
+        context = {'ElementsQuestion':ElementsQuestion}
+        context['note']=note
+        print(Correction)
+    return render(request,'main/correction.html',context)
 
 
 
